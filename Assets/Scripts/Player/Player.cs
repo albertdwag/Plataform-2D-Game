4fa2864568b9e3cc0e76a7bd39d5a9031fed0563
albertdwag <albertcesar463@gmail.com>
2023-03-07 15:26:58 -0300

    adding sprint to the player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,12 +8,14 @@ public class Player : MonoBehaviour
     [SerializeField] private KeyCode moveLeftButton = KeyCode.D;
     [SerializeField] private KeyCode moveRightButton = KeyCode.A;
     [SerializeField] private KeyCode jumpButton = KeyCode.Space;
+    [SerializeField] private KeyCode springButton = KeyCode.LeftControl;
 
     [SerializeField] private Rigidbody2D myRigidbody;
     [SerializeField] private Vector2 friction = new Vector2(-.1f, 0);
     [SerializeField] private float speed;
+    [SerializeField] private float runningSpeed;
     [SerializeField] private float forceJump;
-    [SerializeField] private float maxJumpVelocity;
+    [SerializeField] private float minimumJumpHeight;
 
 
     private void Update()
@@ -25,9 +27,9 @@ public class Player : MonoBehaviour
     private void HandleMove()
     {
         if (Input.GetKey(moveLeftButton))
-            myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+            myRigidbody.velocity = new Vector2(Input.GetKey(springButton) ? runningSpeed : speed, myRigidbody.velocity.y);
         else if (Input.GetKey(moveRightButton))
-            myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
+            myRigidbody.velocity = new Vector2(Input.GetKey(springButton) ? -runningSpeed : -speed, myRigidbody.velocity.y);
 
         if (Mathf.Abs(myRigidbody.velocity.x) < 0.1f)
             myRigidbody.velocity = new Vector2(0, myRigidbody.velocity.y);
@@ -43,7 +45,7 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(jumpButton) && Mathf.Abs(myRigidbody.velocity.y) < 0.001f)
             myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, forceJump);
 
-        else if (Input.GetKeyUp(jumpButton) && myRigidbody.velocity.y > maxJumpVelocity)
-            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, maxJumpVelocity);
+        else if (Input.GetKeyUp(jumpButton) && myRigidbody.velocity.y > minimumJumpHeight)
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, minimumJumpHeight);
     }
 }
