3b8b57eca4a39e505a0c8cc7977e3dfd31d0c88f
albertdwag <albertcesar463@gmail.com>
2023-03-13 16:03:24 -0300

    adding collactable coins, singleton and item manager scripts

---
+++ Assets/Scripts/Utils/ItemCollactableBase.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollactableBase : MonoBehaviour
+{
+    public string compareTag = "Player";
+
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag(compareTag))
+            Collect();
+    }
+
+    protected virtual void Collect()
+    {
+        gameObject.SetActive(false);
+        OnCollect();
+    }
+
+    protected virtual void OnCollect() { }
+}
